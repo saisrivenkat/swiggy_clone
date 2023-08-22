91e1e41e31940a4df058c1a3fe1888800ae45113
sri sai venkat <n.srisai1234@gmail.com>
2023-08-22 17:24:06 +0530

    vercel for server

--- server/api/index.js
+++
@@ -1,2 +0,0 @@
-const app=require('../index')
-app();
--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const port = 5000
 const cors = require('cors')
 const db = require('./db')
 db();
-
 app.use(express.json())
 app.use(cors())
+app.use('/',(req,res)=>res.send("hello"));
 app.use('/user',require('./routes/user'))
 app.listen(port,()=>{
     console.log("listening")
